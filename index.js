a56b4138fdbdd105d6eeb0bb417d263a8db6c2f1
Rahul Shaik <dadarahulshaik@gmail.com>
2022-09-07 00:30:05 +0530

    single line of various operations working .

--- index.js
+++ index.js
@@ -1,3 +1,68 @@
+let str='';
 const calc=(num)=>{
-    document.getElementById('textarea').innerHTML = num;
+    
+    if(num != '=')
+    {
+        str+= num;
+        document.getElementById('textarea').innerHTML = str;
+    }
+    else{
+       PEMDAS(str);
+    }
+    
+}
+
+
+const PEMDAS=(value)=>
+{
+  
+    let regex = /\W/g;
+    let op = value.split(regex);
+    let opr = value.match(regex);
+    const result = operation(op,opr);
+    document.getElementById('textarea').innerHTML =result;
+}
+
+const operation = (op,opr)=>
+{
+ console.log(op,opr['+']);
+ let calc = 0;
+ //multiplication
+ for(;;)
+{
+ let i =  opr.findIndex(x=>x == '*');
+ if(i==-1)
+ break;
+ calc = op[i] * op[i+1];
+ opr.splice(i,1);
+ op.splice(i,2,calc)
+ console.log(opr,op);
+}
+calc = 0;
+//addition
+for(;;)
+{
+ let i =  opr.findIndex(x=>x == '+');
+ if(i==-1)
+ break;
+ calc = parseInt(op[i]) + parseInt(op[i+1]);
+ opr.splice(i,1);
+ op.splice(i,2,calc)
+ console.log(opr,op);
+}
+calc = 0;
+//subtraction
+for(;;)
+{
+ let i =  opr.findIndex(x=>x == '-');
+ if(i==-1)
+ break;
+ calc = op[i] - op[i+1];
+ opr.splice(i,1);
+ op.splice(i,2,calc)
+ console.log(opr,op);
+}
+
+ str='';
+ return op[0];
 }
